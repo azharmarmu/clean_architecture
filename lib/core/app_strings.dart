2d01a9dff0291014b7888db1629d9e6f9b0a4ba5
AzharVelotio <azharuddin@velotio.com>
2023-06-27 15:34:23 +0530

    flet

--- lib/core/app_strings.dart
+++
@@ -1,8 +0,0 @@
-class AppStrings {
-  AppStrings._(); //singleton class
-
-  static String fressery = 'Fressery Products';
-  static String productLabel = 'Products';
-    static String catergoriesLabel = 'Categrories';
-  static String viewAll = 'View All';
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,55 @@
 import 'package:flutter/material.dart';
 
-import 'presentation_layer/stories/dashboard/dashboard_page.dart';
-
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.green,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: DashBoardPage(),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Material 3 Key Changes',
+          style: Theme.of(context).textTheme.headlineSmall,
+        ),
+        elevation: 8,
+        shadowColor: Theme.of(context).shadowColor,
+      ),
+      body: Container(
+        width: double.infinity,
+        height: 200,
+        color: Theme.of(context).colorScheme.secondary,
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: Text(
+            'Hello, Material 3!',
+            style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                  color: Colors.white,
+                ),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
